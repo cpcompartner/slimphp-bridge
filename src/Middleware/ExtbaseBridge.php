642328fab08a0c0f3936b740fe94365a0b40baaf
Dragan Balatinac <balatinac@cp-compartner.de>
2025-08-11 11:58:48 +0200

    WIP - bump dependencies to support typo3 12 and 13, resolve deprecated functions

--- src/Middleware/ExtbaseBridge.php
+++ src/Middleware/ExtbaseBridge.php
@@ -49,7 +49,6 @@ class ExtbaseBridge implements MiddlewareInterface
             $GLOBALS['TSFE']->id = $site->getRootPageId();
         }
 
-        $request = $this->bootFrontend($request);
         $this->bootExtbase($request);
 
         return $handler->handle($request);
@@ -57,68 +56,31 @@ class ExtbaseBridge implements MiddlewareInterface
 
     protected function createGlobalTsfe(Site $site, ServerRequestInterface $request): ServerRequestInterface
     {
-        if (version_compare($this->typo3Version, '11.5', '>=')) {
-            $controller = GeneralUtility::makeInstance(
-                TypoScriptFrontendController::class,
-                $this->context,
-                $site,
-                $request->getAttribute('language', $site->getDefaultLanguage()),
-                new PageArguments($site->getRootPageId(), '0', []),
-                $request->getAttribute('frontend.user')
-            );
+        $controller = GeneralUtility::makeInstance(
+            TypoScriptFrontendController::class,
+            $this->context,
+            $site,
+            $request->getAttribute('language', $site->getDefaultLanguage()),
+            new PageArguments($site->getRootPageId(), '0', []),
+            $request->getAttribute('frontend.user')
+        );
 
-            $controller->determineId($request);
+        $request = $request->withAttribute('frontend.controller', $controller);
 
-            $request = $request->withAttribute('frontend.controller', $controller);
+        // Make TSFE globally available
+        // @todo deprecate $GLOBALS['TSFE'] once TSFE is retrieved from the
+        //       PSR-7 request attribute frontend.controller throughout TYPO3 core
+        $GLOBALS['TSFE'] = $controller;
 
-            // Make TSFE globally available
-            // @todo deprecate $GLOBALS['TSFE'] once TSFE is retrieved from the
-            //       PSR-7 request attribute frontend.controller throughout TYPO3 core
-            $GLOBALS['TSFE'] = $controller;
-        } else {
-            $GLOBALS['TSFE'] = GeneralUtility::makeInstance(
-                TypoScriptFrontendController::class,
-                null,
-                $site,
-                $request->getAttribute('language'),
-                null,
-                $request->getAttribute('frontend.user')
-            );
-        }
-
-        return $request;
-    }
-
-    protected function bootFrontend(ServerRequestInterface $request): ServerRequestInterface
-    {
-        if (version_compare($this->typo3Version, '12.2', '>=')) {
-            // Run Frontend TypoScript
-            $request = $GLOBALS['TSFE']->getFromCache($request);
-        } elseif (version_compare($this->typo3Version, '11.5', '>=')) {
-            // nothing to do, TSFE is already ready
-        } else {
-            $GLOBALS['TSFE']->fetch_the_id($request);
-            $GLOBALS['TSFE']->getConfigArray($request);
-            $GLOBALS['TSFE']->settingLanguage($request);
-            $GLOBALS['TSFE']->newCObj();
-        }
         return $request;
     }
 
     protected function bootExtbase(ServerRequestInterface $request): void
     {
-        if (version_compare($this->typo3Version, '12.4', '>=')) {
-            GeneralUtility::makeInstance(Bootstrap::class)->initialize([
-               'extensionName' => 'slimphp',
-               'vendorName' => 'B13',
-               'pluginName' => 'slimphp',
-           ], $request);
-        }else {
-            GeneralUtility::makeInstance(Bootstrap::class)->initialize([
-               'extensionName' => 'slimphp',
-               'vendorName' => 'B13',
-               'pluginName' => 'slimphp',
-           ]);
-        }
+        GeneralUtility::makeInstance(Bootstrap::class)->initialize([
+            'extensionName' => 'slimphp',
+            'vendorName' => 'B13',
+            'pluginName' => 'slimphp',
+        ], $request);
     }
 }
